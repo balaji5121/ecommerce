393c263d6bf28b6c3bba869af902a2628f08cd85
Balaji Pavan Kumar <101182094+balaji511@users.noreply.github.com>
2023-02-18 15:22:50 +0530

    ML-1234
    button changed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,47 +1,72 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./Navbar.css"
-import { setSearchValue } from '../../redux/reducers/searchReducer'
-import { category } from '../../redux/reducers/filterReducer'
-import { useDispatch, useSelector } from 'react-redux'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { setSearchValue } from "../../redux/reducers/searchReducer";
+import { category } from "../../redux/reducers/filterReducer";
+import { useDispatch, useSelector } from "react-redux";
 
 function Navbar() {
-    const dispatch = useDispatch()
-    const productsCategory = useSelector(state => state.filter.category)
-    
-    return (
-        <nav className='navbar'>
-            <button 
-                className={productsCategory === "all" ? "navLink navLink-active" : 'navLink'}
-                onClick={() => {
-                    dispatch(category("all"))
-                    dispatch(setSearchValue(""))
-                }}
-            ><Link className='nav-list' to="/filterproducts">All</Link></button>
-            <button 
-                className={productsCategory === "clothing" ? "navLink navLink-active" : 'navLink'}
-                onClick={() => {
-                    dispatch(category("clothing"))
-                    dispatch(setSearchValue(""))
-                    
-                }}
-            ><Link  className='nav-list' to="/filterproducts">Fashion</Link></button>
-            <button
-                className={productsCategory === "electronics" ? "navLink navLink-active" : 'navLink'}
-                onClick={() => {
-                    dispatch(category("electronics"))
-                    dispatch(setSearchValue(""))
-                }}
-            ><Link  className='nav-list' to="/filterproducts">Electronics</Link></button>
-            <button 
-                className={productsCategory === "jewelery" ? "navLink navLink-active" : 'navLink'}
-                onClick={() =>{
-                    dispatch(category("jewelery"))
-                    dispatch(setSearchValue(""))
-                }}
-            ><Link  className='nav-list' to="/filterproducts">Jewelery</Link></button>
-        </nav>
-    )
+  const dispatch = useDispatch();
+  const productsCategory = useSelector((state) => state.filter.category);
+
+  return (
+    <nav className="navbar">
+      <button
+        className={
+          productsCategory === "all" ? "navLink navLink-active" : "navLink"
+        }
+        onClick={() => {
+          dispatch(category("all"));
+          dispatch(setSearchValue(""));
+
+          history.pushState("/");
+        }}
+      >
+        ALL
+      </button>
+      <button
+        className={
+          productsCategory === "clothing" ? "navLink navLink-active" : "navLink"
+        }
+        onClick={() => {
+          dispatch(category("clothing"));
+          dispatch(setSearchValue(""));
+        }}
+      >
+        <Link className="nav-list" to="/filterproducts">
+          Fashion
+        </Link>
+      </button>
+      <button
+        className={
+          productsCategory === "electronics"
+            ? "navLink navLink-active"
+            : "navLink"
+        }
+        onClick={() => {
+          dispatch(category("electronics"));
+          dispatch(setSearchValue(""));
+        }}
+      >
+        <Link className="nav-list" to="/filterproducts">
+          Electronics
+        </Link>
+      </button>
+      <button
+        className={
+          productsCategory === "jewelery" ? "navLink navLink-active" : "navLink"
+        }
+        onClick={() => {
+          dispatch(category("jewelery"));
+          dispatch(setSearchValue(""));
+        }}
+      >
+        <Link className="nav-list" to="/filterproducts">
+          Jewelery
+        </Link>
+      </button>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
